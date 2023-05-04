f12e09fd9f81f6e19fa3af0a2262fa514c72031a
Andreas Stocker <stoand@protonmail.com>
2023-05-04 13:34:10 +0200

    ui: get account balance

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,16 +2,21 @@
     import { WsProvider, ApiPromise } from "@polkadot/api";
 
     const ALEPH_ZERO_TESTNET_WS = "wss://ws.test.azero.dev";
+    const ACCOUNT = '5DCwWjuUJ8EaExG5iH94WQ3bq5rtLRhxwYCEsKU2Ry9atLHm';
+
+    let free = '';
 
     async function init() {
         const wsProvider = new WsProvider(ALEPH_ZERO_TESTNET_WS);
         const polkadot = await ApiPromise.create({ provider: wsProvider });
+        const account = await polkadot.query.system.account(ACCOUNT);
+        free = account.data.free.toString();
     }
 
     init();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
 <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+
+    Account<br> <b>{ACCOUNT}</b> has<br><b>{free}</b> free balance.
 </p>
