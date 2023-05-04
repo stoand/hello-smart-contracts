5001e9d25cd74ff0d5f9e489dea9bcd70c03feba
Andreas Stocker <stoand@protonmail.com>
2023-05-04 16:05:26 +0200

    ui: can send signed transactions

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
     import { WsProvider, ApiPromise } from "@polkadot/api";
+    import { keyring } from '@polkadot/ui-keyring';
     import { ContractPromise } from "@polkadot/api-contract";
-    import { create_animation } from "svelte/internal";
+    import {
+        web3Accounts,
+        web3Enable,
+        web3EnablePromise,
+        web3FromSource,
+    } from "@polkadot/extension-dapp";
     import { CONTRACT_META } from "./contract-meta";
 
     const ALEPH_ZERO_TESTNET_WS = "wss://ws.test.azero.dev";
     const ACCOUNT = "5DCwWjuUJ8EaExG5iH94WQ3bq5rtLRhxwYCEsKU2Ry9atLHm";
     const CONTRACT = "5CvxXq49hTR9HFtuCbnWbFe37UpKKSDWukx2nDHtDdPcXaKh";
-
     let free = "";
 
     async function init() {
@@ -17,19 +22,37 @@
         const contract = new ContractPromise(api, CONTRACT_META, CONTRACT);
 
         const account = await api.query.system.account(ACCOUNT);
+        // const accountPair = keyring.getPair(ACCOUNT);
         free = account.data.free.toString();
 
-        let gasLimitV2 = api.registry.createType("WeightV2", {
+        let gasLimit = api.registry.createType("WeightV2", {
             refTime: 3912368128,
             proofSize: 131072,
         }) as any;
 
         let { result, output } = await contract.query.get(ACCOUNT, {
-            gasLimit: gasLimitV2,
+            gasLimit,
             storageDepositLimit: null,
         });
         console.log(result.toHuman());
         console.log(output?.toHuman());
+
+        const extensions = await web3Enable("attendance-manager");
+        if (extensions.length > 0) {
+            const accounts = await web3Accounts();
+            keyring.loadAll({isDevelopment: true}, accounts);
+
+            let account = keyring.getAccounts().find(acc => acc.address == ACCOUNT);
+
+            const injector = await web3FromSource(account.meta.source);
+
+            await contract.tx.inc({
+                gasLimit,
+                storageDepositLimit: null,
+            }, 33).signAndSend(account.address, { signer: injector.signer }, result => {
+                console.log('got', result);
+            });
+        }
     }
 
     init();
