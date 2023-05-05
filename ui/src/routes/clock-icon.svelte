15149a1f724a9483b6d4843a6715307fefead858
Andreas Stocker <stoand@protonmail.com>
2023-05-05 14:56:04 +0200

    ui: top bar partially complete

---
+++ ui/src/routes/clock-icon.svelte
@@ -0,0 +1,28 @@
+<svg width="200" height="200">
+    <g
+        xmlns="http://www.w3.org/2000/svg"
+        id="clock-icon"
+        transform="matrix(0.677693,0,0,0.677693,87.1255,-200.918)"
+    >
+        <g transform="matrix(1,0,0,1,-1128.67,0)">
+            <circle
+                cx="1176.87"
+                cy="461.137"
+                r="78.863"
+                style="fill:none;stroke:white;stroke-width:11.47px;"
+            />
+        </g>
+        <g transform="matrix(1,0,0,1,-1128.67,0)">
+            <path
+                d="M1176.87,461.137L1176.87,402.564L1176.87,461.137Z"
+                style="fill:none;stroke:white;stroke-width:15.36px;"
+            />
+        </g>
+        <g transform="matrix(1,0,0,1,-1128.67,0)">
+            <path
+                d="M1218.32,461.137L1176.87,461.137"
+                style="fill:none;stroke:white;stroke-width:15.36px;"
+            />
+        </g>
+    </g>
+</svg>
