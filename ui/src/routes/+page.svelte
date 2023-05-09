2c8ea474115a8028039c9cec4732bc3171ea58d9
Andreas Stocker <stoand@protonmail.com>
2023-05-09 16:23:45 +0200

    ui: need extension status

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -2,14 +2,19 @@
     import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
     import { polkadotIcon } from "@polkadot/ui-shared";
 
-    let inited = false;
+    let status : 'needExtension' | 'loading' | 'loaded' = 'loading';
     let accounts: any = [];
 
     async function init() {
-        await web3Enable("attendance-manager");
-        accounts = await web3Accounts();
+        let extensions = await web3Enable("attendance-manager");
 
-        inited = true;
+        if (extensions.length == 0) {
+           status = 'needExtension'; 
+        } else {
+            accounts = await web3Accounts();
+
+            status = 'loaded';
+        }
     }
 
     init();
@@ -19,8 +24,11 @@
     <div class="text-5xl ml-16 mt-20">Konto Auswählen</div>
 
     <div class="ml-16 mt-32">
-        {#if !inited}
+        {#if status == 'loading'}
             <div class="text-3xl">Konten werden geladen ...</div>
+        {:else if status == 'needExtension'}
+            <div class="text-3xl">Bitte <a class="underline" target="_blank" href="https://polkadot.js.org/extension/">
+                Extension Installieren</a></div>
         {:else}
             {#each accounts as account}
                 <a
