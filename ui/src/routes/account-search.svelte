991cd6a3331e9fddc4e5a3c75ab33d18672c36af
Andreas Stocker <stoand@protonmail.com>
2023-05-11 15:05:12 +0200

    ui: admin - suggestions - in progress

--- ui/src/routes/account-search.svelte
+++ ui/src/routes/account-search.svelte
@@ -1,16 +1,49 @@
 <script lang="ts">
-    export let account = '';
+    export let account = "";
+
+    export let accounts = [{name: "PeskyRabbit", address: "1"}, {name: "Artichoke", address: "1"}];
 </script>
 
 <div class="ml-16">
-    
     <div class="text-3xl mt-20">Konto Id</div>
 
-    <div class="flex mt-6 ">
-        <input bind:value={account} class="text-2xl p-2 rounded-2xl rounded bg-transparent border-solid border-[1px]"
-            type="text" />
-        <a href="/admin/{account}" class="text-2xl p-2 rounded-2xl rounded bg-transparent border-solid border-[1px] ml-6">
+    <div class="flex mt-6 relative">
+        <input
+            bind:value={account}
+            class="text-2xl p-2 rounded-2xl rounded bg-transparent border-solid border-[1px]"
+            type="text"
+        />
+        <div
+            class="suggestions text-2xl p-2 rounded-2xl rounded bg-blue border-solid border-[1px]"
+        >
+            {#each accounts as account}
+            <a href="/admin/{account.address}" class="block border-b border-solid p-2">{account.name}</a>
+            {/each}
+        </div>
+
+        <a
+            href="/admin/{account}"
+            class="text-2xl p-2 rounded-2xl rounded bg-transparent border-solid border-[1px] ml-6"
+        >
             Anwesenheit Laden
         </a>
     </div>
 </div>
+
+<style>
+    .suggestions {
+        position: absolute;
+        left: 0;
+        top: 40px;
+        z-index: 9999;
+        display: none;
+    }
+
+    input:focus ~ .suggestions {
+        display: block;
+    }
+
+    .suggestions > a:last-child {
+        border: none; 
+    }
+</style>
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -4,13 +4,13 @@ export default {
   theme: {
     colors: {
         white: '#fff',
+        'blue': '#00bcb5',
         'transparent': 'rgba(255,255,255, 0.3)',
         'white-transparent': 'rgba(255,255,255, 0.3)',
         'white-transparent2': 'rgba(255,255,255, 0.5)',
         'disabled': 'rgba(120,120,120, 1)',
         'disabled-transparent': 'rgba(55,55,55, 0.1)',
         'status-good': '#bfff9c',
-        'asdf': 'green',
     },
     extend: {},
   },
